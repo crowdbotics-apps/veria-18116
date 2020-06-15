57ead84e9d2dccb2ea8b93cc2236756b462061f1
Crowdbotics <team@crowdbotics.com>
2020-06-15 10:50:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/veria_18116/MainActivity.java
@@ -0,0 +1,15 @@
+package com.veria_18116;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "veria_18116";
+    }
+}
